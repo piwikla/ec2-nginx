1be72311294e5999adab735965ff47de7f7f7b29
piwik-la <admin@piwik.la>
2014-04-02 01:56:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,17 +22,6 @@ chkconfig php-fpm on
 service mysqld start
 mysql_secure_installation
 
-#Install PECL
-apt-get install php5-geoip php5-dev libgeoip-dev
-pecl install geoip
-
-#LOAD DATA INFILE
-apt-get install php5-mysqlnd
-
-#Install phpMyAdmin
-apt-get install phpmyadmin
-sudo ln -s /usr/share/phpmyadmin/ /home/piwik/public_html/piwik.la
-
 #config nginx
 cp /etc/nginx/sites-available/default /etc/nginx/sites-available/piwik.la
 ln -s /etc/nginx/sites-available/piwik.la /etc/nginx/sites-enabled
@@ -46,9 +35,6 @@ cd piwik
 mv * ../
 cd ../
 rm -rf piwik
-
-#Basic Security
-apt-get install fail2ban iptables-persistent
 
 #Start Nginx
 service php-fpm start
